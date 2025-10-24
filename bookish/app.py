4b36d0fecc96aa279da986ed222201c70334e8ec
DavDob <david.dobrin@softwire.com>
2025-10-24 16:55:18 +0300

    Added authentification controller file and modified the app and __init__.py so that it can be used

--- bookish/app.py
+++ bookish/app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from bookish.models import db, migrate
 from bookish.controllers import register_controllers
+from flask_jwt_extended import JWTManager
 
 
 def create_app():
@@ -9,10 +10,12 @@ def create_app():
 
     app.config.from_object(os.environ['APP_SETTINGS'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 's3cr3t_k3y')
 
     db.init_app(app)
     migrate.init_app(app, db)
 
+    JWTManager(app)
     register_controllers(app)
 
     if __name__ == "__main__":
--- bookish/controllers/__init__.py
+++ bookish/controllers/__init__.py
@@ -1,6 +1,8 @@
 from bookish.controllers.bookish import bookish_routes
 from bookish.controllers.books_controller import book_routes
+from .auth_controller import bp as auth_bp
 
 def register_controllers(app):
+    app.register_blueprint(auth_bp)
     bookish_routes(app)
     book_routes(app)
